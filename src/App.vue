f5fbf2fd8fe247640b3dba108f27b3edc64581dd
Carlos Ferreira <carlos@especializati.com.br>
2023-07-13 17:18:21 -0300

    feat: define base template

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,99 @@
 </script>
 
 <template>
-  clean
+  <header class="header_player">
+    <div class="header_home">
+
+    </div>
+    <nav class="button-group">
+      <button class="button button--outline">
+        Previous Lesson</button>
+      <button class="button button--primary">Complete and Continue</button>
+    </nav>
+  </header>
+  <main>
+    <iframe src="https://player.vimeo.com/video/841243728?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+      frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen
+      title="Young.Sheldon.S05E09.1080p.WEB-DL.DUAL" class="player"></iframe>
+    <aside class="menu">
+      <div class="menu__title">
+        <h1>Aula - React</h1>
+        <div class="status__bar">
+          <div class="loading-background status__completed"></div>
+        </div>
+      </div>
+      <ul class="menu__category-list scrollable-element">
+        <li class="category-list__card">
+          <div class="category-card__title">
+            <h1>Módulo 1</h1>
+          </div>
+          <ul class="category-card__task-list">
+            <li class="task-list__card">
+              <input type="checkbox" name="completed" id="">
+              <p>Welcome to React.js</p>
+            </li>
+            <li class="task-list__card">
+              <input type="checkbox" name="completed" id="">
+              <p>States and immutability</p>
+            </li>
+            <li class="task-list__card">
+              <input type="checkbox" name="completed" id="">
+              <p>States and immutability</p>
+            </li>
+            <li class="task-list__card">
+              <input type="checkbox" name="completed" id="">
+              <p>States and immutability</p>
+            </li>
+            <li class="task-list__card">
+              <input type="checkbox" name="completed" id="">
+              <p>States and immutability</p>
+            </li>
+            <li class="task-list__card">
+              <input type="checkbox" name="completed" id="">
+              <p>States and immutability</p>
+            </li>
+            <li class="task-list__card">
+              <input type="checkbox" name="completed" id="">
+              <p>States and immutability</p>
+            </li>
+            <li class="task-list__card">
+              <input type="checkbox" name="completed" id="">
+              <p>States and immutability</p>
+            </li>
+            <li class="task-list__card">
+              <input type="checkbox" name="completed" id="">
+              <p>States and immutability</p>
+            </li>
+            <li class="task-list__card">
+              <input type="checkbox" name="completed" id="">
+              <p>States and immutability</p>
+            </li>
+            <li class="task-list__card">
+              <input type="checkbox" name="completed" id="">
+              <p>States and immutability</p>
+            </li>
+            <li class="task-list__card">
+              <input type="checkbox" name="completed" id="">
+              <p>States and immutability</p>
+            </li>
+          </ul>
+        </li>
+        <li class="category-list__card">
+          <div class="category-card__title">
+            <h1>Módulo 2</h1>
+          </div>
+          <ul class="category-card__task-list">
+            <li class="task-list__card">
+              <input type="checkbox" name="completed" id="">
+              <p>States and immutability</p>
+            </li>
+            <li class="task-list__card">
+              <input type="checkbox" name="completed" id="">
+              <p>States and immutability</p>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </aside>
+  </main>
 </template>
