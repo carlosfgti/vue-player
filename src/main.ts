f07c02a3fcdefe16481a4457fda95666b381bf1b
Carlos Ferreira <carlos@especializati.com.br>
2023-07-13 17:25:21 -0300

    style: import styles

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import './assets/main.css'
+import './assets/button.css'
+import './assets/environments.css'
+import './assets/header.css'
+import './assets/menu.css'
+import './assets/player.css'
+import './assets/reset.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
