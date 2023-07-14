7e8b3dcb6d151660224833f73688c338d5deafc0
Carlos Ferreira <carlos@especializati.com.br>
2023-07-14 15:40:12 -0300

    feat: create entity lesson

---
+++ src/entities/Lesson.ts
@@ -0,0 +1,14 @@
+export default class Lesson {
+    constructor(
+      readonly name: string,
+      readonly url: string,
+      readonly video: string,
+      readonly published: boolean,
+      readonly description: string,
+      readonly date: string,
+      readonly duration: string,
+      readonly origin: string,
+      readonly free: boolean,
+      readonly created_at: string,
+    ) {}
+}
