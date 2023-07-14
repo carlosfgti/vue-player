3e5d278ce991117bc7c4480357adfbe5e17ee867
Carlos Ferreira <carlos@especializati.com.br>
2023-07-14 15:12:32 -0300

    style: refactor import css in local file

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
-import HeaderComponent from './components/Player/HeaderComponent.vue';
-import PlayerComponent from './components/Player/PlayerComponent.vue';
-import ContentComponent from './components/Player/ContentComponent.vue';
+import '@/assets/main.css'
+import '@/assets/button.css'
+import '@/assets/environments.css'
+import '@/assets/menu.css'
+import '@/assets/reset.css'
+
+import HeaderComponent from '@/components/Player/HeaderComponent.vue';
+import PlayerComponent from '@/components/Player/PlayerComponent.vue';
+import ContentComponent from '@/components/Player/ContentComponent.vue';
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,3 @@
-import './assets/main.css'
-import './assets/button.css'
-import './assets/environments.css'
-import './assets/menu.css'
-import './assets/reset.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
