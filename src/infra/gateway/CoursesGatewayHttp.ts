17f9d5de1ac137b0d48e5e6500878e3f7be570f0
Carlos Ferreira <carlos@especializati.com.br>
2023-07-14 16:40:28 -0300

    feat: get course from api

--- src/infra/gateway/CoursesGatewayHttp.ts
+++ src/infra/gateway/CoursesGatewayHttp.ts
@@ -1,4 +1,4 @@
-import type Course from "@/entities/Course";
+import Course from "@/entities/Course";
 import type CoursesGateway from "./CoursesGateway";
 import type HttpClient from "../http/HttpClient";
 import AxiosAdapter from "../http/AxiosAdapter";
@@ -11,8 +11,24 @@ export default class CoursesGatewayHttp implements CoursesGateway {
 	) {
         this.httpClient = new AxiosAdapter()
     }
-    get(): Promise<Course> {
-        throw new Error("Method not implemented.");
+
+    async get(id: string): Promise<Course> {
+        const response = await this.httpClient.get(`${this.baseUrl}/${id}`);
+        const data = response.data;
+        return new Course(
+            data.name,
+            data.url,
+            data.description,
+            data.certificate_time,
+            data.color,
+            data.origin,
+            data.video,
+            data.image,
+            data.img_social,
+            data.status,
+            data.free,
+            data.date,
+        );
     }
 
     getAll(): Promise<Course[]> {
