efcb217ccb3c574d546c5ff67e15b4bb261cdc46
Carlos Ferreira <carlos@especializati.com.br>
2023-07-14 15:55:48 -0300

    feat: create adapter axios

---
+++ src/infra/http/AxiosAdapter.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import type HttpClient from "./HttpClient";
+
+export default class AxiosAdapter implements HttpClient {
+    async post(url: string, body: any): Promise<any> {
+        const response = await axios.post(url, body);
+		return response.data;
+    }
+
+	async get(url: string): Promise<any> {
+		const response = await axios.get(url);
+		return response.data;
+	}
+
+}
