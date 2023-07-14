4605f63740a2dfe397487efac2fbe08f17eddcd7
Carlos Ferreira <carlos@especializati.com.br>
2023-07-14 15:21:56 -0300

    refactor: use pinia

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,4 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-const pinia = createPinia()
-const app = createApp(App)
-app.use(pinia)
-app.mount('#app')
+createApp(App).use(createPinia()).mount('#app')
