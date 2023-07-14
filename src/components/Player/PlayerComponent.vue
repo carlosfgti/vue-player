b62d79af0ad2dee9d8653aa984db54ea27c72454
Carlos Ferreira <carlos@especializati.com.br>
2023-07-14 15:10:02 -0300

    style: refactor import css in local file

--- src/components/Player/PlayerComponent.vue
+++ src/components/Player/PlayerComponent.vue
@@ -1,3 +1,7 @@
+<script lang="ts">
+import '@/assets/player.css'
+</script>
+
 <template>
     <iframe src="https://player.vimeo.com/video/841243728?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
         frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import './assets/button.css'
 import './assets/environments.css'
 import './assets/header.css'
 import './assets/menu.css'
-import './assets/player.css'
 import './assets/reset.css'
 
 import { createApp } from 'vue'
