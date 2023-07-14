e8b3131f59f0a7466fe91367e25b6c6394905fd1
Carlos Ferreira <carlos@especializati.com.br>
2023-07-14 15:40:45 -0300

    feat: relationship modules with lessons

--- src/entities/Module.ts
+++ src/entities/Module.ts
@@ -1,6 +1,9 @@
+import type Lesson from "./Lesson";
+
 export default class Module {
     constructor(
         readonly name: string,
         readonly description: string,
+        readonly lessons ?: Lesson[]
     ) {}
 }
