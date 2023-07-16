0ebb7c513d0ea7f55fac3e5804d594edd90ebd6b
Carlos Ferreira <carlos@especializati.com.br>
2023-07-16 08:35:25 -0300

    style: format code

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@ import '@/assets/environments.css'
 import '@/assets/menu.css'
 import '@/assets/reset.css'
 
-import HeaderComponent from '@/components/Player/HeaderComponent.vue';
-import PlayerComponent from '@/components/Player/PlayerComponent.vue';
-import ContentComponent from '@/components/Player/ContentComponent.vue';
+import HeaderComponent from '@/components/Player/HeaderComponent.vue'
+import PlayerComponent from '@/components/Player/PlayerComponent.vue'
+import ContentComponent from '@/components/Player/ContentComponent.vue'
 </script>
 
 <template>
