5c1a4559f55fb4b5d2a43a478279fb962be168c4
Carlos Ferreira <carlos@especializati.com.br>
2023-07-16 08:36:46 -0300

    style: format code

--- src/components/Player/HeaderComponent.vue
+++ src/components/Player/HeaderComponent.vue
@@ -3,14 +3,11 @@ import '@/assets/header.css'
 </script>
 
 <template>
-    <header class="header_player">
-        <div class="header_home">
-
-        </div>
-        <nav class="button-group">
-            <button class="button button--outline">
-                Previous Lesson</button>
-            <button class="button button--primary">Complete and Continue</button>
-        </nav>
-    </header>
+  <header class="header_player">
+    <div class="header_home"></div>
+    <nav class="button-group">
+      <button class="button button--outline">Previous Lesson</button>
+      <button class="button button--primary">Complete and Continue</button>
+    </nav>
+  </header>
 </template>
