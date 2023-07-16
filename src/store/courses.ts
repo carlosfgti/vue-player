e7dd29cc306b3cfb36386b1593a2cb0e768d2fb1
Carlos Ferreira <carlos@especializati.com.br>
2023-07-16 08:21:37 -0300

    refactor: fix name var

--- src/store/courses.ts
+++ src/store/courses.ts
@@ -2,18 +2,16 @@ import Course from '@/entities/Course'
 import CoursesGatewayHttp from '@/infra/gateway/CoursesGatewayHttp'
 import { defineStore } from 'pinia'
 
-const CoursesGateway = new CoursesGatewayHttp('/courses');
+const coursesGateway = new CoursesGatewayHttp('/courses')
 
 export const useCounterStore = defineStore('courses', {
-    state: () => ({
-        courses: [] as Course[],
-    }),
-    getters: {
-      
-    },
-    actions: {
-      async getAllCourses() {
-        this.courses = await CoursesGateway.getAll();
-      }
-    },
+  state: () => ({
+    courses: [] as Course[]
+  }),
+  getters: {},
+  actions: {
+    async getAllCourses() {
+      this.courses = await coursesGateway.getAll()
+    }
+  }
 })
