8cf8ebf5a2570a2f5909b03e24e3ceff317b2687
Carlos Ferreira <carlos@especializati.com.br>
2023-07-16 08:34:48 -0300

    style: format code

--- src/components/Player/PlayerComponent.vue
+++ src/components/Player/PlayerComponent.vue
@@ -3,7 +3,12 @@ import '@/assets/player.css'
 </script>
 
 <template>
-    <iframe src="https://player.vimeo.com/video/841243728?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-        frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen
-        title="Young.Sheldon.S05E09.1080p.WEB-DL.DUAL" class="player"></iframe>
+  <iframe
+    src="https://player.vimeo.com/video/841243728?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+    frameborder="0"
+    allow="autoplay; fullscreen; picture-in-picture"
+    allowfullscreen
+    title="Young.Sheldon.S05E09.1080p.WEB-DL.DUAL"
+    class="player"
+  ></iframe>
 </template>
