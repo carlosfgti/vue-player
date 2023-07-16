9ec0483dde1aaa1bd2be6fdd5827dd4529bee20b
Carlos Ferreira <carlos@especializati.com.br>
2023-07-16 08:36:29 -0300

    style: format code

--- src/components/Player/ContentComponent.vue
+++ src/components/Player/ContentComponent.vue
@@ -1,82 +1,82 @@
 <template>
-    <aside class="menu">
-        <div class="menu__title">
-            <h1>Aula - React</h1>
-            <div class="status__bar">
-                <div class="loading-background status__completed"></div>
-            </div>
+  <aside class="menu">
+    <div class="menu__title">
+      <h1>Aula - React</h1>
+      <div class="status__bar">
+        <div class="loading-background status__completed"></div>
+      </div>
+    </div>
+    <ul class="menu__category-list scrollable-element">
+      <li class="category-list__card">
+        <div class="category-card__title">
+          <h1>Módulo 1</h1>
         </div>
-        <ul class="menu__category-list scrollable-element">
-            <li class="category-list__card">
-                <div class="category-card__title">
-                    <h1>Módulo 1</h1>
-                </div>
-                <ul class="category-card__task-list">
-                    <li class="task-list__card">
-                        <input type="checkbox" name="completed" id="">
-                        <p>Welcome to React.js</p>
-                    </li>
-                    <li class="task-list__card">
-                        <input type="checkbox" name="completed" id="">
-                        <p>States and immutability</p>
-                    </li>
-                    <li class="task-list__card">
-                        <input type="checkbox" name="completed" id="">
-                        <p>States and immutability</p>
-                    </li>
-                    <li class="task-list__card">
-                        <input type="checkbox" name="completed" id="">
-                        <p>States and immutability</p>
-                    </li>
-                    <li class="task-list__card">
-                        <input type="checkbox" name="completed" id="">
-                        <p>States and immutability</p>
-                    </li>
-                    <li class="task-list__card">
-                        <input type="checkbox" name="completed" id="">
-                        <p>States and immutability</p>
-                    </li>
-                    <li class="task-list__card">
-                        <input type="checkbox" name="completed" id="">
-                        <p>States and immutability</p>
-                    </li>
-                    <li class="task-list__card">
-                        <input type="checkbox" name="completed" id="">
-                        <p>States and immutability</p>
-                    </li>
-                    <li class="task-list__card">
-                        <input type="checkbox" name="completed" id="">
-                        <p>States and immutability</p>
-                    </li>
-                    <li class="task-list__card">
-                        <input type="checkbox" name="completed" id="">
-                        <p>States and immutability</p>
-                    </li>
-                    <li class="task-list__card">
-                        <input type="checkbox" name="completed" id="">
-                        <p>States and immutability</p>
-                    </li>
-                    <li class="task-list__card">
-                        <input type="checkbox" name="completed" id="">
-                        <p>States and immutability</p>
-                    </li>
-                </ul>
-            </li>
-            <li class="category-list__card">
-                <div class="category-card__title">
-                    <h1>Módulo 2</h1>
-                </div>
-                <ul class="category-card__task-list">
-                    <li class="task-list__card">
-                        <input type="checkbox" name="completed" id="">
-                        <p>States and immutability</p>
-                    </li>
-                    <li class="task-list__card">
-                        <input type="checkbox" name="completed" id="">
-                        <p>States and immutability</p>
-                    </li>
-                </ul>
-            </li>
+        <ul class="category-card__task-list">
+          <li class="task-list__card">
+            <input type="checkbox" name="completed" id="" />
+            <p>Welcome to React.js</p>
+          </li>
+          <li class="task-list__card">
+            <input type="checkbox" name="completed" id="" />
+            <p>States and immutability</p>
+          </li>
+          <li class="task-list__card">
+            <input type="checkbox" name="completed" id="" />
+            <p>States and immutability</p>
+          </li>
+          <li class="task-list__card">
+            <input type="checkbox" name="completed" id="" />
+            <p>States and immutability</p>
+          </li>
+          <li class="task-list__card">
+            <input type="checkbox" name="completed" id="" />
+            <p>States and immutability</p>
+          </li>
+          <li class="task-list__card">
+            <input type="checkbox" name="completed" id="" />
+            <p>States and immutability</p>
+          </li>
+          <li class="task-list__card">
+            <input type="checkbox" name="completed" id="" />
+            <p>States and immutability</p>
+          </li>
+          <li class="task-list__card">
+            <input type="checkbox" name="completed" id="" />
+            <p>States and immutability</p>
+          </li>
+          <li class="task-list__card">
+            <input type="checkbox" name="completed" id="" />
+            <p>States and immutability</p>
+          </li>
+          <li class="task-list__card">
+            <input type="checkbox" name="completed" id="" />
+            <p>States and immutability</p>
+          </li>
+          <li class="task-list__card">
+            <input type="checkbox" name="completed" id="" />
+            <p>States and immutability</p>
+          </li>
+          <li class="task-list__card">
+            <input type="checkbox" name="completed" id="" />
+            <p>States and immutability</p>
+          </li>
         </ul>
-    </aside>
+      </li>
+      <li class="category-list__card">
+        <div class="category-card__title">
+          <h1>Módulo 2</h1>
+        </div>
+        <ul class="category-card__task-list">
+          <li class="task-list__card">
+            <input type="checkbox" name="completed" id="" />
+            <p>States and immutability</p>
+          </li>
+          <li class="task-list__card">
+            <input type="checkbox" name="completed" id="" />
+            <p>States and immutability</p>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </aside>
 </template>
