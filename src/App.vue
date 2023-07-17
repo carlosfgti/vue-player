6b842b121a9ba653103ba2a4245824ba4c19fa32
Carlos Ferreira <carlos@especializati.com.br>
2023-07-17 09:50:29 -0300

    feat: load all courses from store

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,14 @@ import '@/assets/reset.css'
 import HeaderComponent from '@/components/Player/HeaderComponent.vue'
 import PlayerComponent from '@/components/Player/PlayerComponent.vue'
 import ContentComponent from '@/components/Player/ContentComponent.vue'
+import { useCoursesStore } from '@/store/courses'
+import { onMounted } from 'vue'
+
+const courseStore = useCoursesStore()
+
+onMounted(() => {
+  courseStore.getAllCourses()
+})
 </script>
 
 <template>
