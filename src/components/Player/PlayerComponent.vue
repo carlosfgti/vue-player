2b7bc1fd9a38ce44b317f7d2c2b5ca1cab6e304c
Carlos Ferreira <carlos@especializati.com.br>
2023-08-07 17:04:53 -0300

    feat: show lesson in player

--- src/components/Player/PlayerComponent.vue
+++ src/components/Player/PlayerComponent.vue
@@ -1,19 +1,36 @@
 <script lang="ts">
 import '@/assets/player.css'
+import { useLessonsStore } from '@/store/lessons'
 
 export default {
   name: 'PlayerComponent',
   setup() {
+    const lessonStore = useLessonsStore()
+
+    return {
+      lessonStore
+    }
   },
 }
 </script>
 
 <template>
   <iframe
+    v-if="lessonStore.lessonPlayer === null"
+    src="117908430"
+    title="EspecializaTi"
     frameborder="0"
     allow="autoplay; fullscreen; picture-in-picture"
     allowfullscreen
-    title="Young.Sheldon.S05E09.1080p.WEB-DL.DUAL"
+    class="player"
+  ></iframe>
+  <iframe
+    v-else
+    :src="'https://player.vimeo.com/video/' + lessonStore.lessonPlayer.video"
+    :title="lessonStore.lessonPlayer.url"
+    frameborder="0"
+    allow="autoplay; fullscreen; picture-in-picture"
+    allowfullscreen
     class="player"
   ></iframe>
 </template>
