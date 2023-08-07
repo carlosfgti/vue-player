45b195ebdc48bd2527efb94ee46a612442d3d500
Carlos Ferreira <carlos@especializati.com.br>
2023-08-07 15:58:53 -0300

    refactor: add module in lesson entity

--- src/entities/Lesson.ts
+++ src/entities/Lesson.ts
@@ -1,15 +1,18 @@
+import type Module from './Module'
+
 export default class Lesson {
-    constructor(
-      readonly name: string,
-      readonly url: string,
-      readonly video: string,
-      readonly published: boolean,
-      readonly description: string,
-      readonly date: string,
-      readonly duration: string,
-      readonly origin: string,
-      readonly free: boolean,
-      readonly created_at: string,
-      readonly views: number,
-    ) {}
+  constructor(
+    readonly name: string,
+    readonly url: string,
+    readonly video: string,
+    readonly published: boolean,
+    readonly description: string,
+    readonly date: string,
+    readonly duration: string,
+    readonly origin: string,
+    readonly free: boolean,
+    readonly created_at: string,
+    readonly views: number,
+    readonly module?: Module
+  ) {}
 }
