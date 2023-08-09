60d4ff9f64ab284d52b2162a857e11d9e37fb180
Carlos Ferreira <carlos@especializati.com.br>
2023-08-09 17:21:29 -0300

    feat: create login page

--- src/views/AuthPage.vue
+++ src/views/AuthPage.vue
@@ -1,8 +1,39 @@
 <script lang="ts">
+import router from '@/router';
+import { ref } from 'vue'
+import { useUsersStore } from '@/store/users'
+
 export default {
   name: 'AuthPage',
   setup() {
+    const email = ref("")
+    const password = ref("")
+    const loading = ref(false)
 
+    const useStore = useUsersStore()
+
+    const auth = () => {
+            loading.value = true
+
+            useStore.login(email.value, password.value)
+            .then(() => router.push({name: 'player'}))
+            .catch(error => {
+                let msgError = 'Falha na requisição'
+
+                if (error.status === 422) msgError = 'Dados Inválidos'
+                if (error.status === 404) msgError = 'Usuário Não Encontrado'
+
+                alert('Falha ao autenticar')
+            })
+            .finally(() => loading.value = false)
+          }
+
+    return {
+      email,
+      password,
+      loading,
+      auth
+    }
   }
 }
 </script>
@@ -10,9 +41,16 @@ export default {
 <template>
   <div>
     <form action="" method="post">
-      <input type="email" name="email" placeholder="Seu E-mail">
-      <input type="password" name="password" placeholder="Sua Senha">
-      <button type="submit">Entrar</button>
+      <input type="email" name="email" placeholder="Seu E-mail" required v-model="email">
+      <input type="password" name="password" placeholder="Sua Senha" required v-model="password">
+      <button type="submit" :disabled="loading" :class="[
+                                    'btn',
+                                    loading ? 'disabled' : ''
+                                ]"
+                                @click.prevent="auth">
+                                <span v-if="loading">Enviando...</span>
+                                <span v-else>Entrar</span>
+                              </button>
     </form>
   </div>
 </template>
