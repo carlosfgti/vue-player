1bec692721ed4ef08b680df217192099120607e3
João Augusto <jhonaugustjunior@gmail.com>
2023-08-31 15:29:35 -0300

    feat: finish asideComponent UI

--- src/components/Player/AsideComponent.vue
+++ src/components/Player/AsideComponent.vue
@@ -14,9 +14,7 @@ export default {
     const toMyCourses = () => router.push({ name: 'my.courses' })
 
     onMounted(() => {
-      if (courseStore.courseSelected === null) {
-        return toMyCourses()
-      }
+      if (courseStore.courseSelected === null) return toMyCourses()
     })
 
     const openForum = () => window.open('https://academy.especializati.com.br/forum', '_blank')
@@ -56,6 +54,14 @@ export default {
       <div class="info">
         <h3 class="title">{{ courseStore.courseSelected?.name }}</h3>
         <span>{{ `${Math.floor(courseStore.totalCourseComplete)}%` }} concluído</span>
+        <ul role="list">
+          <li role="button" @click="toMyCourses" title="Meus cursos">
+            <i class="fas fa-books"></i>
+          </li>
+          <li role="button" @click="openForum" title="Abrir fórum">
+            <i class="fab fa-discourse"></i>
+          </li>
+        </ul>
       </div>
     </div>
     <hr />
@@ -87,42 +93,6 @@ export default {
         </li>
       </ul>
     </div>
-    <!-- <h1>{{ lessonStore.title }}</h1> -->
-
-    <!-- <div v-if="courseStore.courseSelected === null">Carregando...</div> -->
-    <!-- <ul v-else class="menu__category-list scrollable-element">
-      <li
-        v-for="(module, index) in courseStore.courseSelected.modules"
-        :key="index"
-        class="category-list__card"
-      >
-        <div class="category-card__title">
-          <h1>{{ module.name }}</h1>
-        </div>
-        <ul class="category-card__task-list" v-if="module.lessons !== null">
-          <li v-for="lesson in module.lessons" :key="lesson.url" class="task-list__card">
-            <input type="checkbox" name="completed" id="" :checked="lesson.views > 0" />
-            <p @click.prevent="lessonStore.addLessonPlayer(lesson)" style="cursor: pointer">
-              {{ lesson.name }}
-            </p>
-          </li>
-        </ul>
-      </li>
-      <div class="footer-sidebar">
-        <h2 class="category-card__title">Outros links</h2>
-        <ul>
-          <li class="task-list__card">
-            <p @click="toMyCourses" style="cursor: pointer">Meus Cursos</p>
-          </li>
-          <li class="task-list__card">
-            <p @click="openForum">Fórum</p>
-          </li>
-          <li class="task-list__card">
-            <p @click="logout" style="cursor: pointer">Sair</p>
-          </li>
-        </ul>
-      </div>
-    </ul> -->
   </aside>
 </template>
 
